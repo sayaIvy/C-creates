e9fc3bcb8997dc8589bb391a8c67d07f269dfffb
sayaIvy <saya6414h@gmail.com>
2022-12-05 21:54:53 +0900

    1week list4-2 インスタンス

--- 1weekC++/list4/main.cpp
+++ 1weekC++/list4/main.cpp
@@ -1,10 +1,18 @@
 #include "car.h"
+#include <iostream>
 
+using namespace std;
 int main()
 {
-    Car c;
-    c.supply(10); //燃料補給
-    c.move();     //移動
-    c.move();     //移動
+    // new,deleteを使えばコンストラクタ、デストラクタの
+    //タイミングを任意にできる
+    Car *pC = 0;    //ポインタは０かNULL
+    pC = new Car(); //インスタンス生成
+    pC->supply(10); //燃料補給
+    pC->move();     //移動
+    pC->move();     //移動
+    delete pC;      //インスタンスの削除
+    cout << "インスタンスの消去完了" << endl;
     return 0;
 }
+// mallocやfreeはコンストラクタを呼び出せないから使わない
