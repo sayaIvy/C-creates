b4a089a202a04c94d151e2b4c72373e8c0ce154c
sayaIvy <saya6414h@gmail.com>
2022-12-11 13:01:20 +0900

    moveInConsole

--- games/guruTeto/main.cpp
+++ games/guruTeto/main.cpp
@@ -17,10 +17,11 @@
 // }
 
 #include <stdio.h>
-#define clearScreen() printf("/033[2]")
-#define setPosition(x, y) printf("/033[%d;%dH", (y), (x))
-#define setCharColor(n) printf("/033[3%dm", (n))
-#define setBackColor(n) printf("/033[4%dm", (n))
+#include <time.h>
+#define clearScreen() printf("\e[2]")
+#define setPosition(x, y) printf("\e[%d;%dH", (y), (x))
+#define setCharColor(n) printf("\e[3%dm", (n))
+#define setBackColor(n) printf("\e[4%dm", (n))
 #define BLACK 0
 #define RED 1
 #define GREEN 2
@@ -31,14 +32,28 @@
 #define WHITE 7
 #define DEFAULT 9
 
+int wait(int msec);
+int wait(int msec)
+{
+    struct timespec r = {0, msec * 1000L * 1000L};
+    return nanosleep(&r, NULL);
+    {
+        /* data */
+    };
+}
 int main(int argc, char *argv[])
 {
+    int y;
     clearScreen();
     setBackColor(BLUE);
     setCharColor(YELLOW);
-    setPosition(5, 10);
-    printf("hello!");
-    fflush(stdout);
+    for (y = 1; y < 23; y++)
+    {
+        setPosition(5, y);
+        printf("$");
+        fflush(stdout);
+        wait(500);
+    }
     setBackColor(DEFAULT);
     setCharColor(DEFAULT);
 }
--- games/guruTeto/otamesi/escapeSequence.sh
+++ games/guruTeto/otamesi/escapeSequence.sh
@@ -5,3 +5,11 @@ for ((i = 0; i < 16; i++)); do
     done
     echo "";
 done
+
+for ((i = 0; i < 16; i++)); do
+    for ((j = 0; j < 16; j++)); do
+        hex=$(($i*16 + $j))
+        printf '\e[48;5;%dm0x%02X\e[m ' $hex $hex
+    done
+    echo "";
+done
