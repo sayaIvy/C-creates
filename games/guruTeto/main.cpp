ad2782b734b79c7a432709301a74f1fdd0cd36f8
sayaIvy <saya6414h@gmail.com>
2022-12-21 14:36:58 +0900

    ブロックの追加

--- games/guruTeto/main.cpp
+++ games/guruTeto/main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <time.h>
+
+#define WIDTH 10
+#define HEIGHT 20
+
 #define clearScreen() printf("\e[2J")
-#define setPosition(x, y) printf("\e[%d;%dH", (y), (x))
+#define setPosition(x, y) printf("\e[%d;%dH", (y) + 1, (x)*2 + 1) // 座標を(0,0)からにする
+#define cursorOn() printf("\e[?25h")                              // カーソルを表示
+#define cursorOff() printf("\e[?25i")                             // 　カーソルを非表示
+#define FEILD_WIDTH 10
+#define FEILD_HIGHT 20
 #define setCharColor(n) printf("\e[3%dm", (n))
 #define setBackColor(n) printf("\e[4%dm", (n))
 #define BLACK 0
@@ -24,39 +32,242 @@
 #define HIDE 8     // 隠す
 #define STRIKE 9   // 取り消し線
 
+#define BLOCK_SIZE 4
+#define BLOCK_NUM 7
+
+typedef struct cell
+{
+    char c; // 表示文字
+    int charColor;
+    int backColor;
+    int attribute; // 文字属性
+} Cell;
+
+Cell block_type[BLOCK_NUM][BLOCK_SIZE][BLOCK_SIZE] = {
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    ' ', RED, BLACK, REVERSE,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    ' ', RED, BLACK, REVERSE,
+    ' ', RED, BLACK, REVERSE,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+    '\0', RED, BLACK, NORMAL,
+};
+
 int wait(int msec);
+void initialize(void); // 画面初期化
+void reset(void);      // 画面の復元
+int checkRange(Cell a, int x, int y);
+int printCell(Cell c, int x, int y);
+int clearCell(Cell c, int x, int y);
+void copyBlock(Cell src[BLOCK_SIZE][BLOCK_SIZE], Cell dst[BLOCK_SIZE][BLOCK_SIZE]);
+int printBlock(Cell block[BLOCK_SIZE][BLOCK_SIZE], int x, int y);
+int clearBlock(Cell block[BLOCK_SIZE][BLOCK_SIZE], int x, int y);
+
 int main(int argc, char *argv[])
 {
     int y;
-    clearScreen();
-    for (y = 1; y < 23; y++)
+    Cell a = {' ', WHITE, BLACK, REVERSE};
+    initialize();
+    for (y = 1; y < HEIGHT; y++)
     {
-        setPosition(5, y);
-        setCharColor(WHITE);
-        setBackColor(BLACK);
-        setAttribute(REVERSE);
-        printf("  ");
-        fflush(stdout);
-
+        printCell(a, 5, y);
         wait(500);
-
-        setPosition(5, y);
-        setCharColor(WHITE);
-        setBackColor(BLACK);
-        setAttribute(NORMAL);
-        printf("  ");
-        fflush(stdout);
+        clearCell(a, 5, y);
     }
-    setBackColor(DEFAULT);
-    setCharColor(DEFAULT);
-    clearScreen();
+    reset();
 }
 
 int wait(int msec)
 {
     struct timespec r = {0, msec * 1000L * 1000L};
     return nanosleep(&r, NULL);
-    {
-        /* data */
-    };
+}
+
+void initialize(void)
+{
+    setBackColor(BLACK);
+    setCharColor(WHITE);
+    setAttribute(NORMAL);
+    clearScreen();
+    cursorOff();
+}
+
+void reset(void)
+{
+    setBackColor(BLACK);
+    setCharColor(WHITE);
+    setAttribute(NORMAL);
+    clearScreen();
+    cursorOn();
+}
+
+int checkRange(Cell a, int x, int y)
+{
+    if (a.c == '\0' || x < 0 || y < 0 || x >= FEILD_WIDTH || y >= FEILD_HIGHT)
+        return -1; // 失敗
+    else
+        return 0; // 成功
+}
+
+int printCell(Cell c, int x, int y)
+{
+    if (checkRange(c, x, y) == -1)
+        return -1;
+    setPosition(x, y);
+    setAttribute(c.attribute);
+    setBackColor(c.backColor);
+    setCharColor(c.charColor);
+    printf("%c%c", c.c, c.c);
+    fflush(stdout);
+    return 0;
+}
+
+int clearCell(Cell c, int x, int y)
+{
+    if (checkRange(c, x, y) == -1)
+        return -1;
+    setPosition(x, y);
+    setAttribute(NORMAL);
+    setBackColor(BLACK);
+    setCharColor(BLACK);
+    printf("  ");
+    fflush(stdout);
+    return 0;
+}
+
+void copyBlock(Cell src[BLOCK_SIZE][BLOCK_SIZE], Cell dst[BLOCK_SIZE][BLOCK_SIZE])
+{
+    int i, j;
+    for (j = 0; j < BLOCK_SIZE; j++)
+        for (i = 0; i < BLOCK_SIZE; i++)
+            dst[j][i] = src[j][i];
+}
+
+int printBlock(Cell block[BLOCK_SIZE][BLOCK_SIZE], int x, int y);
+{
+    int i, j;
+    for (j = 0; j < BLOCK_SIZE; j++)
+        for (i = 0; i < BLOCK_SIZE; i++)
+            printCell(block[j][i], i + x, j + y);
+    return 0;
+}
+
+int clearBlock(Cell block[BLOCK_SIZE][BLOCK_SIZE], int x, int y);
+{
+    int i, jj;
+    for (j = 0; j < BLOCK_SIZE; j++)
+        for (i = 0; i < BLOCK_SIZE; i++)
+            clearCell(block[j][i], i + x, j + y);
+    return 0;
 }
--- games/guruTeto/otamesi/escapeSequence.sh
+++ games/guruTeto/otamesi/escapeSequence.sh
@@ -1,3 +1,10 @@
+# background
+# lightblue 0x7B
+# white 0xE7
+# gray 0xF6
+# black 0xE8
+
+
 for ((i = 0; i < 16; i++)); do
     for ((j = 0; j < 16; j++)); do
         hex=$(($i*16 + $j))
@@ -10,14 +17,11 @@ for ((i = 0; i < 16; i++)); do
     for ((j = 0; j < 16; j++)); do
         hex=$(($i*16 + $j))
         printf '\e[48;5;%dm0x%02X\e[m ' $hex $hex
+        printf '%dm0x%02' $hex $hex
     done
     echo "";
 done
 
-
-
-sleep 3
-printf '\e[2J';
-printf '\e[2K';
-printf '\e[1m\e[7m';
-echo "";
+for ((a = 0; a < 20; a++)); do
+    printf '\e[48;5;%dm0x%02X0xF6\e[m ' $hex $hex
+done
